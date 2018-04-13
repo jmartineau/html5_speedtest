e84dc9212182b1c61dab95154b5b9ee7b6441a57
jmartineau <jmartineau@csumb.edu>
2018-04-13 11:14:25 -0700

    Initial commit

---
+++ telemetry_settings.php
@@ -0,0 +1,20 @@
+<?php
+
+$db_type="mysql"; //Type of db: "mysql", "sqlite" or "postgresql"
+
+// Sqlite3 settings
+$Sqlite_db_file = "../telemetry.sql";
+
+// Mysql settings
+$MySql_username="jmartineau";
+$MySql_password="";
+$MySql_hostname="localhost";
+$MySql_databasename="html5_speedtest";
+
+// Postgresql settings
+$PostgreSql_username="USERNAME";
+$PostgreSql_password="PASSWORD";
+$PostgreSql_hostname="DB_HOSTNAME";
+$PostgreSql_databasename="DB_NAME";
+
+?>
